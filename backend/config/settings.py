b913d948bc8b78837c8a472e1c43754cdb703bb1
Sushant <140909146+sushantmalviya@users.noreply.github.com>
2026-08-20 11:33:00 +0530

    fix render deployment errors

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -173,6 +173,7 @@ USE_TZ = True
 # --------------------------------------------------
  
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
  
